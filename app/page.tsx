811207b43db6379ad53d1368bb8fe74c99ed1d77
toriyamaiori <iori.wakuwaku.programing@gmail.com>
2025-12-03 11:19:22 +0900

    page.tsxも動的レンダリングをするようにした

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Calendar from './calendar';
 import ArticleList from './articlelist';
 import Image from 'next/image';
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
 
 
